6e5953834153b38e577b2b8b12055dc5144bbb3f
Peter Venables <pvenables@gmail.com>
2019-03-13 19:34:25 -0400

    add atbash cypher and test

---
+++ cypher/atbash.py
@@ -0,0 +1,56 @@
+import string
+
+class AtbashCypher:
+    """
+    AtbashCypher - a form of the affine cypher but with fixed registration
+                   such that a = z, b = y, ... z = a
+    """
+
+    def encode(in_str):
+        """ encodes a string using the atbash cypher. 
+        
+        Arguments:
+          in_str (string): a string to encode
+
+        Returns:
+          string: the encoded string
+
+        """
+        rev = list(string.ascii_lowercase)
+        rev.reverse()
+        out_str = ''
+        for ch in in_str:
+          if ch.isalpha():
+            ch_idx = string.ascii_lowercase.index(ch.lower())
+            if ch.isupper():
+              out_str += str(rev[ch_idx].upper())
+            else: 
+              out_str += str(rev[ch_idx])
+          else:
+            out_str += str(ch)
+        return out_str
+
+    def decode(in_str):
+        """ decodes a string using the atbash cypher.
+        
+        Arguments:
+          in_str (string): a string to decode
+
+        Returns:
+          string: the decoded string
+          
+        """
+        rev = list(string.ascii_lowercase)
+        rev.reverse()
+        out_str = ''
+        for ch in in_str:
+          if ch.isalpha():
+            ch_idx = rev.index(ch.lower())
+            if ch.isupper():
+              out_str += str(string.ascii_uppercase[ch_idx].upper())
+            else:
+              out_str += str(string.ascii_lowercase[ch_idx])
+          else:
+            out_str += str(ch)
+        return out_str
+
---
+++ test/test_atbash.py
@@ -0,0 +1,14 @@
+import unittest
+from cypher.atbash import AtbashCypher as abc
+
+class AtbashCypherTests(unittest.TestCase):
+
+    def test_encode(self):
+        test_string = 'Test this'
+        test_result = 'Gvhg gsrh'
+        self.assertEqual(abc.encode(test_string), test_result)
+
+    def test_decode(self):
+        test_string = 'Gvhg gsrh'
+        test_result = 'Test this'
+        self.assertEqual(abc.decode(test_string), test_result)
